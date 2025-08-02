877e408b2cdb3b155191cdd32561a4519e3fd2a0
joaofvic <fjoao2012v@gmail.com>
2025-08-01 21:53:50 -0300

    implemented interfaces

---
+++ src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+import model.services.PaypalService;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		Scanner sc = new Scanner(System.in);		
+		System.out.println("Among the contract data:");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		
+		System.out.print("Date (dd/MM/yyyy): ");
+		LocalDate date = LocalDate.parse(sc.next(), fmt);
+		
+		System.out.print("Contract value: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract obj = new Contract(number, date, totalValue);
+		
+		System.out.print("Enter the number of installments: ");
+		int n = sc.nextInt();
+		
+		ContractService service = new ContractService(new PaypalService());
+		service.processContract(obj, n);
+		
+		System.out.println("Installments:");
+		for (Installment installments : obj.getInstallments()) {
+			System.out.println(installments);
+		}
+		sc.close();
+	}
+}
